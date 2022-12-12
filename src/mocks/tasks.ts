5de47eb9aadedc90da8bc9d8150037e268865c39
pedro-drosa <pedro_drosa@hotmail.com>
2022-12-12 07:40:57 -0300

    feat: add mock tasks

---
+++ src/mocks/tasks.ts
@@ -0,0 +1,22 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export default [
+  {
+    id: uuidv4(),
+    content:
+      'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+    finished: false,
+  },
+  {
+    id: uuidv4(),
+    content:
+      'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+    finished: false,
+  },
+  {
+    id: uuidv4(),
+    content:
+      'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+    finished: true,
+  },
+];
